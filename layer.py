391feabb4cc469ad99f3d1fbfb60787ce7cff14a
Hari Kumar <harik1@MacChickenNugget.local>
2023-09-03 17:48:28 -0600

    fix

--- layer.py
+++ layer.py
@@ -1,12 +1,16 @@
 import numpy as np
 
 class Layer:
-    def __init__(self, p_neuron: int=1, c_neuron: int=1, activation = lambda z: max(0, z)):
+    def __init__(self, p_neuron: int=1, c_neuron: int=1, p_layer: object=None, activation = lambda z: max(0, z)):
         self.weight = np.random.rand(p_neuron, c_neuron)
         self.bias = np.random.rand(c_neuron)
+        self.size = c_neuron
         self.activation = activation
         self.z = np.array()
         self.a = np.array()
+        self.next = None
+        self.prev = p_layer
+
     
     def forward(self, input):
         self.z = self.weights.dot(input) + self.biases
