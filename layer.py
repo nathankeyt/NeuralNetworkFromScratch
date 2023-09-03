5d7fb31b95a6f4c86f3a49f912203b9c721ee68c
secu1760 <secu1760@colorado.edu>
2023-09-03 16:14:06 -0600

    layer beginning

--- layer.py
+++ layer.py
@@ -1,5 +1,9 @@
 import numpy as np
 
 class Layer:
-    def __init__(self, p_neuron: int=1, c_neuron: int=1, activation: function=lambda z: max(0, z)):
+    def __init__(self, p_neuron: int=1, c_neuron: int=1, activation = lambda z: max(0, z)):
+        self.weights = np.random.rand(p_neuron, c_neuron)
+        print(self.weights)
+        
         print("layer")
+        
--- neural_network.py
+++ neural_network.py
@@ -1,7 +1,10 @@
 import numpy as np
+from layer import Layer 
 
 class NeuralNetwork:
     def __init__(self, num_inputs: int=1, hidden_layers: list=[1], num_ouputs: list=1):
+        np.array([1,2,3,4,5])
+        Layer(2, 3)
         print("constructor")
     
     def train(self, input_data: list, labels: list, epochs: int, learning_rate: float, batch_size: int):
