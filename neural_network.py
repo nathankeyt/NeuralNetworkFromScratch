427418648c100c81a785354ee93be79109d66901
Nathan Keyt <nathankeyt@Nathans-MacBook-Pro-2.local>
2023-09-03 13:38:21 -0600

    neural network class structure

---
+++ neural_network.py
@@ -0,0 +1,9 @@
+class NeuralNetwork:
+    def __init__(num_inputs: int, hidden_layers: list, num_ouputs: list):
+        print("constructor")
+    
+    def train(input_data: list, labels: list, epochs: int, learning_rate: float, batch_size: int):
+        print("train")
+
+    def test(input_data: list, labels: list):
+        print("test")
