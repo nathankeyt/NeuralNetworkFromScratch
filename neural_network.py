67826da32c3eef020c9f16212ee701a2524412f5
Nathan Keyt <nathankeyt@Nathans-MacBook-Pro-2.local>
2023-09-03 13:45:40 -0600

    default args nn class

--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,5 @@
 class NeuralNetwork:
-    def __init__(self, num_inputs: int, hidden_layers: list, num_ouputs: list):
+    def __init__(self, num_inputs: int = 1, hidden_layers: list = [1], num_ouputs: list = 1):
         print("constructor")
     
     def train(self, input_data: list, labels: list, epochs: int, learning_rate: float, batch_size: int):
