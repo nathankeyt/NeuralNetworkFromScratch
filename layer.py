5890b488769c4e5cc91e09ed2292939a4324c2d2
secu1760 <secu1760@colorado.edu>
2023-09-03 16:23:10 -0600

    bias

--- layer.py
+++ layer.py
@@ -3,7 +3,7 @@ import numpy as np
 class Layer:
     def __init__(self, p_neuron: int=1, c_neuron: int=1, activation = lambda z: max(0, z)):
         self.weights = np.random.rand(p_neuron, c_neuron)
-        print(self.weights)
+        self.bias = np.random.rand(c_neuron)
         
         print("layer")
         
