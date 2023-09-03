31458f3af0b20a1f4e36650d4d238d43902910cc
Nathan Keyt <nathankeyt@Nathans-MacBook-Pro-2.local>
2023-09-03 16:32:50 -0600

    layer to layer forward prop function

--- layer.py
+++ layer.py
@@ -2,8 +2,14 @@ import numpy as np
 
 class Layer:
     def __init__(self, p_neuron: int=1, c_neuron: int=1, activation = lambda z: max(0, z)):
-        self.weights = np.random.rand(p_neuron, c_neuron)
+        self.weight = np.random.rand(p_neuron, c_neuron)
         self.bias = np.random.rand(c_neuron)
-        
-        print("layer")
+        self.activation = activation
+        self.z = np.array()
+        self.a = np.array()
+    
+    def forward_prop(self, input):
+        self.z = self.weights.dot(input) + self.biases
+        self.a = self.activation(self.z)
+        return self.a
         
